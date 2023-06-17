73047c9c3dbda740fda95a1d491524ab98e0c1b2
fcustodio90 <filipe.l.custodio@gmail.com>
2023-06-17 16:29:47 +0100

    Add vuex typescript mutations

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,5 @@
-import { createStore } from "vuex";
+import { createStore, createLogger } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  plugins: process.env.NODE_ENV === "development" ? [createLogger()] : [],
 });
